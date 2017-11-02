b7cf6e292de4b68cc0fe84c89501b8581d40208a
Serhii Kosyouk <monstrpro@gmail.com>
2017-11-02 19:42:27 +0200

    Add ChatInterface & Service

--- example/example.php
+++ example/example.php
@@ -1,1 +1,11 @@
+<?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$chatRoom = new \Chat\Chat();
+$chatService = new \Chat\Service($chatRoom);
+
+$single = $chatService->newChat();
+
+echo '<pre>';
+print_r($single);
--- lib/Service.php
+++ lib/Service.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace Chat;
+
+/**
+ * Class Service
+ * @package Chat
+ */
+class Service
+{
+    /** @var  $chatProvider */
+    protected $chatProvider;
+
+    /**
+     * @param ChatInterface $chatProvider
+     */
+    public function __constructor(ChatInterface $chatProvider)
+    {
+            $this->chatProvider = $chatProvider;
+    }
+
+    /**
+     * @return string
+     */
+    public function newChat()
+    {
+        return $this->chatProvider->getChat();
+    }
+}
